ac30381cfac00899c52ff00bde7ddc719438ce7a
Gerardo Lucero <gera_conecta@hotmail.com>
2019-09-08 12:43:06 -0600

    se creo el portafolio

---
+++ resources/views/web/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<section class="container">
+    <h1>Hola Mundo este es el index</h1>
+</section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,26 @@
 |
 */
 
-// Example Routes
-Route::view('/', 'landing');
-Route::match(['get', 'post'], '/dashboard', function(){
-    return view('dashboard');
+// Web routes
+Route::get('/', 'web\IndexController@index');
+
+Auth::routes();
+
+// System routes
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::match(['get', 'post'], '/dashboard', 'System\SystemController@index');
+
+    // Portfolio routes
+    Route::get('dashboard/portafolio', 'System\PortfolioController@index')->name('portfolio.index');
+    Route::get('dashboard/portafolio/create', 'System\PortfolioController@create')->name('portfolio.create');
+    Route::post('dashboard/portafolio', 'System\PortfolioController@store')->name('portfolio.store');
+    Route::get('dashboard/portafolio/{id}', 'System\PortfolioController@edit')->name('portfolio.edit');
+    Route::put('dashboard/portafolio/{id}', 'System\PortfolioController@update')->name('portfolio.update');
+    Route::delete('dashboard/portafolio/{id}', 'System\PortfolioController@destroy')->name('portfolio.delete');
+
 });
-Route::view('/examples/plugin-helper', 'examples.plugin_helper');
-Route::view('/examples/plugin-init', 'examples.plugin_init');
-Route::view('/examples/blank', 'examples.blank');
+
+
+
+
