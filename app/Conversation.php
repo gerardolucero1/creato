fa431b03c067bc6151848cb522c109ad9cd823af
erik019 <undle40@gmail.com>
2020-01-03 18:20:27 -0700

    02-01-20

--- app/Conversation.php
+++ app/Conversation.php
@@ -3,28 +3,15 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
-
-    protected $appends = ['contact_name', 'profile_photo'];
-    
+    protected $appends = ['contact_name'];
     // Definicion de un asesor
     public function getContactNameAttribute($key)
     {
         return $this->contact()->first(['name'])->name;
-    }
-    // obtener imagen
-    public function getProfilePhotoAttribute($key)
-    {
-        return $this->user()->profile()->first(['photo'])->photo;
     }
     public function contact()
     {
         // N Conversation    1 User (Contact)
         return $this->belongsTo(User::class);
     }
-    public function user()
-    {
-        // N Conversation    1 User (Contact)
-                return $this->belongsToThrough(User::class, Profile::class);
-    }
-    
 }
